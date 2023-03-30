90917d25468e1e1a131d366bc2082a9d5ac3ff63
yineleyici <hooochiba@gmail.com>
2023-03-30 18:40:19 +0900

    rewrites機能

--- next.config.js
+++ next.config.js
@@ -1,10 +1,29 @@
 /**
  * @type {import('next').NextConfig}
  */
+repoName = "/nextjs-blog-learn";
 module.exports = {
-  basePath: process.env.GITHUB_ACTIONS && "/nextjs-blog-learn",
+  basePath: process.env.GITHUB_ACTIONS && repoName,
+  assetPrefix: process.env.GITHUB_ACTIONS && repoName,
   trailingSlash: true,
   images: {
     unoptimized: true,
+  },
+
+  async rewrites() {
+    return [
+      {
+        source: `${repoName}/api/:path*`,
+        destination: "/api/:path*",
+      },
+      {
+        source: `${repoName}/images/:query*`,
+        destination: "/_next/image/:query*",
+      },
+      {
+        source: `${repoName}/_next/:path*`,
+        destination: "/_next/:path*",
+      },
+    ];
   },
 };
