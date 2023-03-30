9172bd09577143bb088cd2d0961313a14ae8576a
yineleyici <hooochiba@gmail.com>
2023-03-30 17:35:44 +0900

    next-optimized-images

--- next.config.js
+++ next.config.js
@@ -2,13 +2,16 @@
  * @type {import('next').NextConfig}
  */
 
+const withOptimizedImages = require("next-optimized-images");
+
 const reponeme = "/nextjs-blog-learn";
 
-module.exports = {
+module.exports = withOptimizedImages({
   basePath: process.env.GITHUB_ACTIONS && reponeme,
   assetPrefix: process.env.GITHUB_ACTIONS && reponeme,
   trailingSlash: true,
+  handleImages: ["jpg"],
   images: {
     unoptimized: true,
   },
-};
+});
