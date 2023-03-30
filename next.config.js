6fa5844fb9a3f93f1a2bf92afb7d9b61ba3a2460
yineleyici <hooochiba@gmail.com>
2023-03-30 16:26:19 +0900

    Revert "deploy.ymlを修正"
    This reverts commit 66860c770beaff48381d09dbe44ce1c52c653679.

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@
 module.exports = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || "",
+
   trailingSlash: true,
   images: {
     unoptimized: true,
