66860c770beaff48381d09dbe44ce1c52c653679
yineleyici <hooochiba@gmail.com>
2023-03-30 16:06:20 +0900

    deploy.ymlを修正

--- next.config.js
+++ next.config.js
@@ -4,7 +4,6 @@
 module.exports = {
   basePath: process.env.NEXT_PUBLIC_BASE_PATH || "",
   assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || "",
-
   trailingSlash: true,
   images: {
     unoptimized: true,
