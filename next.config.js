79c4baa12e1ff750f76e42fd7e4cb5ddf4eb1b6d
yineleyici <hooochiba@gmail.com>
2023-03-30 15:10:42 +0900

    静的サイト生成時の相対パス解決

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 /**
  * @type {import('next').NextConfig}
  */
+
+const isProd = process.env.NODE_ENV === "production";
+const basePath = process.env.GITHUB_ACTIONS && "/nextjs-blog-learn";
+
 module.exports = {
-  basePath: process.env.GITHUB_ACTIONS && "/nextjs-blog-learn",
+  basePath: basePath,
   trailingSlash: true,
   images: {
     unoptimized: true,
   },
+  assetPrefix: isProd ? basePath : "",
 };
