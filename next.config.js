c38b965008d35d418ef5dadf720a77a40da5b20e
yineleyici <hooochiba@gmail.com>
2023-03-30 18:06:58 +0900

    Revert "next-optimized-images"
    This reverts commit 9172bd09577143bb088cd2d0961313a14ae8576a.

--- next.config.js
+++ next.config.js
@@ -2,16 +2,13 @@
  * @type {import('next').NextConfig}
  */
 
-const withOptimizedImages = require("next-optimized-images");
-
 const reponeme = "/nextjs-blog-learn";
 
-module.exports = withOptimizedImages({
+module.exports = {
   basePath: process.env.GITHUB_ACTIONS && reponeme,
   assetPrefix: process.env.GITHUB_ACTIONS && reponeme,
   trailingSlash: true,
-  handleImages: ["jpg"],
   images: {
     unoptimized: true,
   },
-});
+};
