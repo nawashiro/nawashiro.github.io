0432dc9ab6ec77722f7f1b5a971d01255b8f2339
yineleyici <hooochiba@gmail.com>
2023-03-30 18:07:03 +0900

    Revert "Next.js の public 以下のファイルのパスを正しく扱う"
    This reverts commit cc9851f67c676b9d767c4be40428adc266950688.

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /**
  * @type {import('next').NextConfig}
  */
-
-const reponeme = "/nextjs-blog-learn";
-
 module.exports = {
-  basePath: process.env.GITHUB_ACTIONS && reponeme,
-  assetPrefix: process.env.GITHUB_ACTIONS && reponeme,
+  basePath: process.env.GITHUB_ACTIONS && "/nextjs-blog-learn",
   trailingSlash: true,
   images: {
     unoptimized: true,
