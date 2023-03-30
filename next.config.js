cc9851f67c676b9d767c4be40428adc266950688
yineleyici <hooochiba@gmail.com>
2023-03-30 16:59:29 +0900

    Next.js の public 以下のファイルのパスを正しく扱う

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /**
  * @type {import('next').NextConfig}
  */
+
+const reponeme = "/nextjs-blog-learn";
+
 module.exports = {
-  basePath: process.env.GITHUB_ACTIONS && "/nextjs-blog-learn",
+  basePath: process.env.GITHUB_ACTIONS && reponeme,
+  assetPrefix: process.env.GITHUB_ACTIONS && reponeme,
   trailingSlash: true,
   images: {
     unoptimized: true,
