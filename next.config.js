4474b0871714c38a8179a617bc565112b396c01d
nawashiro <hooochiba@gmail.com>
2023-09-26 20:34:46 +0900

    next.config.jsを更新

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
 /**
  * @type {import('next').NextConfig}
  */
-repoName = process.env.GITHUB_ACTIONS && "nawashiro.github.io";
 module.exports = {
-  basePath: repoName,
-  assetPrefix: repoName,
-  trailingSlash: true,
-  publicRuntimeConfig: {
-    basePath: repoName,
-  },
 };
