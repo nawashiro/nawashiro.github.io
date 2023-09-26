87cbbf3eed154830498105185986ec237a895a5e
nawashiro <hooochiba@gmail.com>
2023-09-26 19:23:12 +0900

    baseをindexに追加

--- pages/index.jsx
+++ pages/index.jsx
@@ -19,6 +19,7 @@ export async function getStaticProps() {
 export default function Home({ allPostsData }) {
   return (
     <Layout home>
+      <base href="/" />
       <Head>
         <title>{siteTitle}</title>
       </Head>
