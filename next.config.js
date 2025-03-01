e930ac21d093b975cea51a3033ac3e49985f03e1
nawashiro <hooochiba@gmail.com>
2025-03-02 05:16:21 +0900

    feat: feedジェネレータを追加

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-/**
- * @type {import('next').NextConfig}
- */
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   output: "export",
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // サーバーサイドのビルド時にRSSフィードを生成
+      const { generateRssFeed } = require('./lib/posts');
+      generateRssFeed();
+    }
+    return config;
+  },
 };
+
+module.exports = nextConfig;
