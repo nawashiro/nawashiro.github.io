b66a25fe10ef46f80b81b486e013ea4af6125b98
nawashiro <62463717+nawashiro@users.noreply.github.com>
2026-03-04 12:27:30 +0900

    Modify link elements for PGP and IndieAuth
    Updated link elements for PGP key and IndieAuth metadata to be hidden.

--- pages/index.tsx
+++ pages/index.tsx
@@ -79,8 +79,8 @@ export default function Home({
       >
         github.com/nawashiro
       </a>
-      <link rel="pgpkey" href="/openpgp_publickey.asc" />
-      <link rel="authorization_endpoint" href="https://indielogin.com/auth">
+      <a rel="pgpkey" href="/openpgp_publickey.asc" style={{ display: "none" }} ></a>
+      <a rel="indieauth-metadata" href="https://indielogin.com/auth" style={{ display: "none" }} ></a>
       <div className="h-card" style={{ display: "none" }}>
         <a className="p-name u-url" href={siteUrl}>
           Nawashiro
